44e05f2c05733edc46918f1a7bfd5027261e7144
Swarnab Garang <72680953+0xcatrovacer@users.noreply.github.com>
2022-02-13 21:54:36 +0530

    added test can create a new vibe

--- tests/solana-social.ts
+++ tests/solana-social.ts
@@ -1,17 +1,35 @@
-import * as anchor from '@project-serum/anchor';
-import { Program } from '@project-serum/anchor';
-import { SolanaSocial } from '../target/types/solana_social';
+import * as anchor from "@project-serum/anchor";
+import { Program } from "@project-serum/anchor";
+import { SolanaSocial } from "../target/types/solana_social";
+import * as assert from "assert";
+import * as bs58 from "bs58";
 
-describe('solana-social', () => {
+describe("solana-social", () => {
+    anchor.setProvider(anchor.Provider.env());
 
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+    const program = anchor.workspace.SolanaSocial as Program<SolanaSocial>;
 
-  const program = anchor.workspace.SolanaSocial as Program<SolanaSocial>;
+    it("can create a new vibe", async () => {
+        const vibe = anchor.web3.Keypair.generate();
 
-  it('Is initialized!', async () => {
-    // Add your test here.
-    const tx = await program.rpc.initialize({});
-    console.log("Your transaction signature", tx);
-  });
+        await program.rpc.createVibe("First Vibe!", "Vibe Content!", {
+            accounts: {
+                vibe: vibe.publicKey,
+                author: program.provider.wallet.publicKey,
+                systemProgram: anchor.web3.SystemProgram.programId,
+            },
+            signers: [vibe],
+        });
+
+        const createdVibe = await program.account.vibe.fetch(vibe.publicKey);
+
+        assert.equal(createdVibe.topic, "First Vibe!");
+        assert.equal(createdVibe.content, "Vibe Content!");
+        assert.equal(
+            createdVibe.author.toBase58(),
+            program.provider.wallet.publicKey.toBase58()
+        );
+        assert.equal(createdVibe.likes, 0);
+        assert.ok(createdVibe.timestamp);
+    });
 });
