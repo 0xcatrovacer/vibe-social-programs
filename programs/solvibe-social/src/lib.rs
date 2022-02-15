119460618a6a8820e3f4b342280cf1814a1239af
Swarnab Garang <72680953+0xcatrovacer@users.noreply.github.com>
2022-02-15 21:44:46 +0530

    changed program id

--- programs/solvibe-social/src/lib.rs
+++ programs/solvibe-social/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("3NcczXqNpzUsp6c2pGqLNJMnUwScvZBXduj8f3sg6Qdz");
 
 #[program]
 pub mod solvibe_social {
