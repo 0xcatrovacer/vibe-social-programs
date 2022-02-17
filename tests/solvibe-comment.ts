0a47a642d9b429a034befcbd8ea1cab69ffb823d
Swarnab Garang <72680953+0xcatrovacer@users.noreply.github.com>
2022-02-17 11:26:22 +0530

    added test can delete a comment

--- tests/solvibe-comment.ts
+++ tests/solvibe-comment.ts
@@ -51,4 +51,61 @@ describe("solvibe-comments", () => {
         );
         assert.equal(createdComment.comment, "New Comment");
     });
+
+    it("can delete a comment", async () => {
+        const vibe = anchor.web3.Keypair.generate();
+        const author = program.provider.wallet;
+
+        await program.rpc.createVibe("New Vibe", "Comment to be Added", {
+            accounts: {
+                vibe: vibe.publicKey,
+                author: author.publicKey,
+                systemProgram: anchor.web3.SystemProgram.programId,
+            },
+            signers: [vibe],
+        });
+
+        const [commentAccount, commentBump] =
+            await anchor.web3.PublicKey.findProgramAddress(
+                [
+                    Buffer.from("vibe_comment"),
+                    author.publicKey.toBuffer(),
+                    vibe.publicKey.toBuffer(),
+                ],
+                program.programId
+            );
+
+        await program.rpc.addComment("New Comment", commentBump, {
+            accounts: {
+                comment: commentAccount,
+                vibe: vibe.publicKey,
+                commentor: author.publicKey,
+                systemProgram: anchor.web3.SystemProgram.programId,
+            },
+        });
+
+        const createdComment = await program.account.comment.fetch(
+            commentAccount
+        );
+
+        assert.equal(
+            createdComment.commentor.toBase58(),
+            author.publicKey.toBase58()
+        );
+        assert.ok(createdComment !== null);
+
+        await program.rpc.removeComment(commentBump, {
+            accounts: {
+                comment: commentAccount,
+                vibe: vibe.publicKey,
+                commentor: author.publicKey,
+            },
+        });
+
+        const deleltedComment = await program.account.comment.fetchNullable(
+            commentAccount
+        );
+
+        assert.ok(deleltedComment === null);
+    });
 });
