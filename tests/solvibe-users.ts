2c3d5316f8814ec01b19d3c84e5213d7cececa36
Swarnab Garang <72680953+0xcatrovacer@users.noreply.github.com>
2022-02-18 22:20:10 +0530

    added test can create user

---
+++ tests/solvibe-users.ts
@@ -0,0 +1,40 @@
+import * as anchor from "@project-serum/anchor";
+import { Program } from "@project-serum/anchor";
+import { SolvibeSocial } from "../target/types/solvibe_social";
+import * as assert from "assert";
+
+describe("solvibe-users", () => {
+    anchor.setProvider(anchor.Provider.env());
+
+    const program = anchor.workspace.SolvibeSocial as Program<SolvibeSocial>;
+
+    it("can create user", async () => {
+        const author = program.provider.wallet;
+
+        const username = "unique username";
+
+        const [userAccount, userBump] =
+            await anchor.web3.PublicKey.findProgramAddress(
+                [Buffer.from("vibe_user"), Buffer.from(username)],
+                program.programId
+            );
+
+        await program.rpc.createUser("New Name", username, userBump, {
+            accounts: {
+                userAccount: userAccount,
+                author: author.publicKey,
+                systemProgram: anchor.web3.SystemProgram.programId,
+            },
+        });
+
+        const createdUser = await program.account.user.fetch(userAccount);
+
+        assert.equal(
+            createdUser.userKey.toBase58(),
+            author.publicKey.toBase58()
+        );
+        assert.equal(createdUser.name, "New Name");
+        assert.equal(createdUser.username, "unique username");
+        assert.equal(createdUser.bump, userBump);
+    });
+});
